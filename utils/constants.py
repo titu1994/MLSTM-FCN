9a52bfbf015b35a7598e56ef995320a95319dd7b
Somshubra Majumdar <titu1994@gmail.com>
2017-08-14 12:10:24 -0500

    Begin working on arabic voice dataset

--- utils/constants.py
+++ utils/constants.py
@@ -4,6 +4,7 @@ TRAIN_FILES = ['../data/arabic/', # 0
                '../data/character/', # 2
                '../data/Action3D/', # 3
                '../data/Activity/', # 4
+               '../data/arabic_voice/', # 5
                ]
 
 TEST_FILES = ['../data/arabic/', # 0
@@ -11,6 +12,7 @@ TEST_FILES = ['../data/arabic/', # 0
               '../data/character/', # 2
               '../data/Action3D/', # 3
               '../data/Activity/', # 4
+              '../data/arabic_voice/', # 5
               ]
 
 MAX_TIMESTEPS_LIST = [13, # 0
@@ -18,6 +20,7 @@ MAX_TIMESTEPS_LIST = [13, # 0
                       30, # 2
                       570, # 3
                       570, # 4
+                      39, # 5
                       ]
 
 MAX_NB_VARIABLES = [93,  # 0
@@ -25,6 +28,7 @@ MAX_NB_VARIABLES = [93,  # 0
                     173,  # 2
                     100, # 3
                     337, # 4
+                    91, # 5
                     ]
 
 
@@ -33,4 +37,5 @@ NB_CLASSES_LIST = [10, # 0
                    20, # 2
                    20, # 3
                    16, # 4
+                   88, # 5
                    ]
