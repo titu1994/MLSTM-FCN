a1d1b844bb47bd676332d6ebf7757a0b95748ee8
Somshubra Majumdar <titu1994@gmail.com>
2017-08-15 15:57:00 -0500

    Add CCV

---
+++ data/CCVdatabase/note.md
@@ -0,0 +1,1 @@
+Add CCV dataset files here
--- utils/keras_utils.py
+++ utils/keras_utils.py
@@ -28,8 +28,82 @@ def multi_label_log_loss(y_pred, y_true):
     return K.sum(K.binary_crossentropy(y_pred, y_true), axis=-1)
 
 
+def _average_gradient_norm(model, X_train, y_train, batch_size):
+    # just checking if the model was already compiled
+    if not hasattr(model, "train_function"):
+        raise RuntimeError("You must compile your model before using it.")
+
+    weights = model.trainable_weights  # weight tensors
+
+    get_gradients = model.optimizer.get_gradients(model.total_loss, weights)  # gradient tensors
+
+    input_tensors = [
+        # input data
+        model.inputs[0],
+        # how much to weight each sample by
+        model.sample_weights[0],
+        # labels
+        model.targets[0],
+        # train or test mode
+        K.learning_phase()
+    ]
+
+    grad_fct = K.function(inputs=input_tensors, outputs=get_gradients)
+
+    steps = 0
+    total_norm = 0
+    s_w = None
+
+    nb_steps = X_train.shape[0] // batch_size
+
+    if X_train.shape[0] % batch_size == 0:
+        pad_last = False
+    else:
+        pad_last = True
+
+    def generator(X_train, y_train, pad_last):
+        for i in range(nb_steps):
+            X = X_train[i * batch_size: (i + 1) * batch_size, ...]
+            y = y_train[i * batch_size: (i + 1) * batch_size, ...]
+
+            yield (X, y)
+
+        if pad_last:
+            X = X_train[nb_steps * batch_size:, ...]
+            y = y_train[nb_steps * batch_size:, ...]
+
+            yield (X, y)
+
+    datagen = generator(X_train, y_train, pad_last)
+
+    while steps < nb_steps:
+        X, y = next(datagen)
+        # set sample weights to one
+        # for every input
+        if s_w is None:
+            s_w = np.ones(X.shape[0])
+
+        gradients = grad_fct([X, s_w, y, 0])
+        total_norm += np.sqrt(np.sum([np.sum(np.square(g)) for g in gradients]))
+        steps += 1
+
+    if pad_last:
+        X, y = next(datagen)
+        # set sample weights to one
+        # for every input
+        if s_w is None:
+            s_w = np.ones(X.shape[0])
+
+        gradients = grad_fct([X, s_w, y, 0])
+        total_norm += np.sqrt(np.sum([np.sum(np.square(g)) for g in gradients]))
+        steps += 1
+
+    return total_norm / float(steps)
+
+
+
 def train_model(model:Model, dataset_id, dataset_prefix, dataset_fold_id=None, epochs=50, batch_size=128, val_subset=None,
-                cutoff=None, normalize_timeseries=False, learning_rate=1e-3):
+                cutoff=None, normalize_timeseries=False, learning_rate=1e-3, compile_model=True):
     X_train, y_train, X_test, y_test, is_timeseries = load_dataset_at(dataset_id,
                                                                       fold_index=dataset_fold_id,
                                                                       normalize_timeseries=normalize_timeseries)
@@ -77,7 +151,8 @@ def train_model(model:Model, dataset_id, dataset_prefix, dataset_fold_id=None, e
 
     optm = Adam(lr=learning_rate)
 
-    model.compile(optimizer=optm, loss='categorical_crossentropy', metrics=['accuracy'])
+    if compile_model:
+        model.compile(optimizer=optm, loss='categorical_crossentropy', metrics=['accuracy'])
 
     if val_subset is not None:
         X_test = X_test[:val_subset]
@@ -141,6 +216,34 @@ def set_trainable(layer, value):
    # case: wrapper (which is a case not covered by the PR)
    if hasattr(layer, 'layer'):
         set_trainable(layer.layer, value)
+
+
+def compute_average_gradient_norm(model:Model, dataset_id, dataset_fold_id=None, batch_size=128,
+                cutoff=None, normalize_timeseries=False, learning_rate=1e-3):
+    X_train, y_train, X_test, y_test, is_timeseries = load_dataset_at(dataset_id,
+                                                                      fold_index=dataset_fold_id,
+                                                                      normalize_timeseries=normalize_timeseries)
+    max_timesteps, sequence_length = calculate_dataset_metrics(X_train)
+
+    if sequence_length != MAX_NB_VARIABLES[dataset_id]:
+        if cutoff is None:
+            choice = cutoff_choice(dataset_id, sequence_length)
+        else:
+            assert cutoff in ['pre', 'post'], 'Cutoff parameter value must be either "pre" or "post"'
+            choice = cutoff
+
+        if choice not in ['pre', 'post']:
+            return
+        else:
+            X_train, X_test = cutoff_sequence(X_train, X_test, choice, dataset_id, sequence_length)
+
+    y_train = to_categorical(y_train, len(np.unique(y_train)))
+
+    optm = Adam(lr=learning_rate)
+    model.compile(optimizer=optm, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    average_gradient = _average_gradient_norm(model, X_train, y_train, batch_size)
+    print("Average gradient norm : ", average_gradient)
 
 
 class MaskablePermute(Permute):
