231cc2fcda9237ee5864d120017e4f71c81d45ae
Somshubra Majumdar <titu1994@gmail.com>
2018-09-24 11:37:27 -0700

    Use training loss instead of validation accuracy

--- utils/keras_utils.py
+++ utils/keras_utils.py
@@ -103,7 +103,7 @@ def _average_gradient_norm(model, X_train, y_train, batch_size):
 
 
 def train_model(model:Model, dataset_id, dataset_prefix, dataset_fold_id=None, epochs=50, batch_size=128, val_subset=None,
-                cutoff=None, normalize_timeseries=False, learning_rate=1e-3, monitor='val_acc', optimization_mode='max', compile_model=True):
+                cutoff=None, normalize_timeseries=False, learning_rate=1e-3, monitor='loss', optimization_mode='max', compile_model=True):
     X_train, y_train, X_test, y_test, is_timeseries = load_dataset_at(dataset_id,
                                                                       fold_index=dataset_fold_id,
                                                                       normalize_timeseries=normalize_timeseries)
